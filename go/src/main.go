2754ab1641411e5868b26e42d071e6f4ddd2692b
konekato <18ec033@ms.dendai.ac.jp>
2019-12-28 21:56:10 +0900

    add view file

--- go/src/main.go
+++ go/src/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"html/template"
+	"io"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
 	"handler"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 	// Echo instance
 	e := echo.New()
+
+	e.Renderer = t
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -19,6 +35,7 @@ func main() {
 	e.GET("/user", handler.MainPage())
 	e.GET("/user/get/:id", handler.GetUser())
 	e.POST("/user/post", handler.PostUser())
+	e.GET("/user/page1", handler.Template())
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
